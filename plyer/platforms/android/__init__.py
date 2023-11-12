3873a5d62487be5d510713a2ae4f214f595ceb02
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-12 13:52:26 -0300

    Update __init__.py

--- plyer/platforms/android/__init__.py
+++ plyer/platforms/android/__init__.py
@@ -3,10 +3,7 @@ from java import jclass as autoclass
 
 ANDROID_VERSION = autoclass('android.os.Build$VERSION')
 SDK_INT = ANDROID_VERSION.SDK_INT
-	@@ -14,6 +48,6 @@
-    PythonService = autoclass(ns + '.PythonService')
-    activity = PythonService.mService
-else:
-    PythonActivity = autoclass(ns + '.PythonActivity')
-    print(dir(PythonActivity))
-    activity = PythonActivity
+
+PythonActivity = autoclass(ns + '.PythonActivity')
+print(dir(PythonActivity))
+activity = PythonActivity
