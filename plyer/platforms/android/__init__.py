0d1c7780586cd1116064783d1002c963646114a7
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-12 13:55:22 -0300

    Update __init__.py

--- plyer/platforms/android/__init__.py
+++ plyer/platforms/android/__init__.py
@@ -4,6 +4,15 @@ from java import jclass as autoclass
 ANDROID_VERSION = autoclass('android.os.Build$VERSION')
 SDK_INT = ANDROID_VERSION.SDK_INT
 
-PythonActivity = autoclass('org.beeware.android.PythonActivity')
-print(dir(PythonActivity))
-activity = PythonActivity
+try:
+    from android import config
+    ns = config.JAVA_NAMESPACE
+except (ImportError, AttributeError):
+    ns = 'org.beeware.android'
+
+if 'PYTHON_SERVICE_ARGUMENT' in environ:
+    PythonService = autoclass(ns + '.PythonService')
+    activity = PythonService.mService
+else:
+    PythonActivity = autoclass(ns + '.PythonActivity')
+    activity = PythonActivity
