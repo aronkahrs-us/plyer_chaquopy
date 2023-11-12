03068f800fd9c8dcacc10f84a7e18e480105a2f2
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-12 15:32:38 -0300

    Update __init__.py

--- plyer/platforms/android/__init__.py
+++ plyer/platforms/android/__init__.py
@@ -1,5 +1,6 @@
 from os import environ
 from java import jclass as autoclass
+from com.ak.geosmartphone.geosmartphone import R;
 
 ANDROID_VERSION = autoclass('android.os.Build$VERSION')
 SDK_INT = ANDROID_VERSION.SDK_INT
@@ -14,5 +15,6 @@ if 'PYTHON_SERVICE_ARGUMENT' in environ:
     PythonService = autoclass(ns + '.PythonService')
     activity = PythonService.mService
 else:
+    print(R)
     PythonActivity = autoclass(ns + '.PythonActivity')
     activity = PythonActivity
