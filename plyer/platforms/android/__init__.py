4224c4a067cfd868fff2a92e6d50e2165bc7e891
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-12 13:54:07 -0300

    Update __init__.py

--- plyer/platforms/android/__init__.py
+++ plyer/platforms/android/__init__.py
@@ -4,6 +4,6 @@ from java import jclass as autoclass
 ANDROID_VERSION = autoclass('android.os.Build$VERSION')
 SDK_INT = ANDROID_VERSION.SDK_INT
 
-PythonActivity = autoclass(ns + '.PythonActivity')
+PythonActivity = autoclass('org.beeware.android.PythonActivity')
 print(dir(PythonActivity))
 activity = PythonActivity
