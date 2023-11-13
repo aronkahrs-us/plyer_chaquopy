9689974e0cb742d545d4c5bc1ca06a20d4227933
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-13 18:50:53 -0300

    Update vibrator.py

--- plyer/platforms/android/vibrator.py
+++ plyer/platforms/android/vibrator.py
@@ -2,14 +2,13 @@
 
 from java import jclass as autoclass
 from java import cast as cast
-from java.lang import String
 from plyer.facades import Vibrator
 from plyer.platforms.android import activity
 from plyer.platforms.android import SDK_INT
 
 Context = autoclass("android.content.Context")
 print('before service',activity,dir(activity))
-vibrator_service = activity.getSystemService(String(Context.VIBRATOR_SERVICE))
+vibrator_service = activity.getSystemService(Context.VIBRATOR_SERVICE)
 vibrator = cast("android.os.Vibrator", vibrator_service)
 if SDK_INT >= 26:
     VibrationEffect = autoclass("android.os.VibrationEffect")
