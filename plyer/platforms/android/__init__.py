7ccef9b823953d1648746cac1770715c1a55f055
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-13 18:35:11 -0300

    Update __init__.py

--- plyer/platforms/android/__init__.py
+++ plyer/platforms/android/__init__.py
@@ -1,7 +1,5 @@
 from os import environ
 from java import jclass as autoclass
-from java.lang import String
-from com.ak.geosmartphone.geosmartphone import R;
 
 ANDROID_VERSION = autoclass('android.os.Build$VERSION')
 SDK_INT = ANDROID_VERSION.SDK_INT
