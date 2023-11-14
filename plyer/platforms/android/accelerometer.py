76a06497cf07eb932bb541fd0ad24323cd7be315
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-14 18:05:17 -0300

    Update accelerometer.py

--- plyer/platforms/android/accelerometer.py
+++ plyer/platforms/android/accelerometer.py
@@ -32,10 +32,8 @@ class AccelerometerSensorListener(dynamic_proxy(SensorEventListener)):
     def enable(self):
         self.SensorManager.registerListener(
             self, self.sensor,
-            SensorManager.SENSOR_DELAY_GAME
+            SensorManager.SENSOR_DELAY_NORMAL
         )
-        print('SENSOR_DELAY')
-        print(SensorManager.SENSOR_DELAY_GAME)
 
     def disable(self):
         self.SensorManager.unregisterListener(self, self.sensor)
