6b1b174e5be445a836dfc9e6e063a8d8094f2b4c
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-13 21:28:57 -0300

    Update accelerometer.py

--- plyer/platforms/android/accelerometer.py
+++ plyer/platforms/android/accelerometer.py
@@ -12,6 +12,7 @@ from plyer.platforms.android import activity
 
 Context = autoclass('android.content.Context')
 Sensor = autoclass('android.hardware.Sensor')
+SensorEvent = autoclass('android.hardware.SensorEvent')
 SensorManager = autoclass('android.hardware.SensorManager')
 
 class AccelerometerSensorListener(PythonJavaClass):
@@ -38,7 +39,7 @@ class AccelerometerSensorListener(PythonJavaClass):
     def disable(self):
         self.SensorManager.unregisterListener(self, self.sensor)
 
-    @java_method('void',['Landroid/hardware/SensorEvent'])
+    @java_method('void',[SensorEvent])
     def onSensorChanged(self, event):
         self.values = event.values[:3]
 
