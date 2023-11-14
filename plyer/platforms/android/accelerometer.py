4f217d2dbeefdb174a7e9da8fdbf1b99d779504e
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-11-13 21:20:46 -0300

    Update accelerometer.py

--- plyer/platforms/android/accelerometer.py
+++ plyer/platforms/android/accelerometer.py
@@ -1,4 +1,4 @@
-'''
+h'''
 Android accelerometer
 ---------------------
 '''
@@ -7,7 +7,7 @@ from plyer.facades import Accelerometer
 from java import dynamic_proxy as PythonJavaClass
 from java import jclass as autoclass
 from java import cast as cast
-from java import static_proxy as java_method
+from java import method as java_method
 from plyer.platforms.android import activity
 
 Context = autoclass('android.content.Context')
