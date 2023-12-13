6d2b56705a1223645c7a0cbbdf33506e8f863274
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-12-13 15:55:50 -0300

    Update gyroscope.py

--- plyer/platforms/android/gyroscope.py
+++ plyer/platforms/android/gyroscope.py
@@ -41,8 +41,9 @@ class GyroscopeSensorListener(dynamic_proxy(SensorEventListener)):
         self.SensorManager.unregisterListener(self, self.sensor)
 
     def onSensorChanged(self, event):
-        self.values = event.values[:3]
-        print('GYRO: ',self.values[:3])
+        if event.sensor.getType == Sensor.TYPE_GYROSCOPE:
+            self.values = event.values[:3]
+            print('GYRO: ',self.values[:3])
 
     def onAccuracyChanged(self, sensor, accuracy):
         # Maybe, do something in future?
