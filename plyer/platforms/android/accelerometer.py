dcc641ae683d0ac46e646f6efdad22a69ada9a80
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-12-13 13:42:31 -0300

    Update accelerometer.py

--- plyer/platforms/android/accelerometer.py
+++ plyer/platforms/android/accelerometer.py
@@ -40,6 +40,7 @@ class AccelerometerSensorListener(dynamic_proxy(SensorEventListener)):
         
     def onSensorChanged(self, event):
         self.values = event.values[:3]
+        print('ACC: ',self.values)
 
     def onAccuracyChanged(self, sensor, accuracy):
         # Maybe, do something in future?
