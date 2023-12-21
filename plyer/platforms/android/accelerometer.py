0c17810ecc3db60e7011e09626d25b3a1b229095
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-12-21 19:29:28 -0300

    Update accelerometer.py

--- plyer/platforms/android/accelerometer.py
+++ plyer/platforms/android/accelerometer.py
@@ -27,32 +27,21 @@ class AccelerometerSensorListener(dynamic_proxy(SensorEventListener)):
         self.sensor = self.SensorManager.getDefaultSensor(
             Sensor.TYPE_LINEAR_ACCELERATION
         )
-        self.sensor2 = self.SensorManager.getDefaultSensor(
-            Sensor.TYPE_GYROSCOPE
-        )
 
         self.values = [None, None, None]
-        self.values2 = [None, None, None]
 
     def enable(self):
         self.SensorManager.registerListener(
             self, self.sensor,
             SensorManager.SENSOR_DELAY_FASTEST
         )
-        self.SensorManager.registerListener(
-            self, self.sensor2,
-            SensorManager.SENSOR_DELAY_FASTEST
-        )
 
     def disable(self):
         self.SensorManager.unregisterListener(self, self.sensor)
-        self.SensorManager.unregisterListener(self, self.sensor2)
         
     def onSensorChanged(self, event):
         if event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION:
             self.values = event.values[:3]
-        elif event.sensor.getType() == Sensor.TYPE_GYROSCOPE:
-            self.values2 = event.values[:3]
 
     def onAccuracyChanged(self, sensor, accuracy):
         # Maybe, do something in future?
