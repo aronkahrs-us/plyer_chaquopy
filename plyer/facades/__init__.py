c680995612790445ac299960b967515e5f4e546f
Aron Kahrs <85702110+aronkahrs-us@users.noreply.github.com>
2023-12-24 11:13:55 -0300

    Update __init__.py

--- plyer/facades/__init__.py
+++ plyer/facades/__init__.py
@@ -12,7 +12,7 @@ __all__ = ('Accelerometer', 'Audio', 'Barometer', 'Battery', 'Call', 'Camera',
            'Sms', 'TTS', 'UniqueID', 'Vibrator', 'Wifi', 'Flash', 'CPU',
            'Temperature', 'Humidity', 'SpatialOrientation', 'Brightness',
            'Processors', 'StoragePath', 'Keystore', 'Bluetooth', 'Screenshot',
-           'STT', 'DeviceName', 'Maps')
+           'STT', 'DeviceName', 'Maps', 'MULTI')
 
 from plyer.facades.accelerometer import Accelerometer
 from plyer.facades.audio import Audio
@@ -50,3 +50,4 @@ from plyer.facades.cpu import CPU
 from plyer.facades.screenshot import Screenshot
 from plyer.facades.devicename import DeviceName
 from plyer.facades.maps import Maps
+from plyer.facades.multi import MULTI
